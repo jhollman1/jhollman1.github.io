b3b6153e528e012560af169746f8af8062ad2dd5
jhollman1 <4gitbubby29@gmail.com>
2018-05-14 23:06:27 -0500

    what a mess

---
+++ data.js
@@ -0,0 +1,758 @@
+var calories_march = [
+//march_calories = [
+    {
+	id: 1,
+	total_calories: 0,
+	total_price: 0.00,
+	content: [
+	    {
+		"name": "donuts",
+		"calories": 910,
+		"price": 0.96
+	    },
+	    {
+		"name": "pizza",
+		"calories": 1120,
+		"price": 2.50
+	    },
+	    {
+		"name": "cereal",
+		"calories": 250,
+		"price": 0.23
+	    },
+	    {
+		"name": "sandwich",
+		"calories": 300,
+		"price": 0.52
+	    },
+	    {
+		"name": "beer",
+		"calories": 1472,
+		"price": 10.75
+	    }
+	]
+    },
+    {
+	id: 2,
+	total_calories: 0,
+	total_price: 0.00,
+	content: [
+	    {
+		"name": "donuts",
+		"calories": 350,
+		"price": 0.32
+	    },
+	    {
+		"name": "fries",
+		"calories": 660,
+		"price": 0.75
+	    },
+	    {
+		"name": "pizza",
+		"calories": 2160,
+		"price": 6.57
+	    },
+	    {
+		"name": "sandwich",
+		"calories": 300,
+		"price": 0.52
+	    },
+	    {
+		"name": "beer",
+		"calories": 540,
+		"price": 3.80
+	    }
+	]
+    },
+    {
+	id: 3,
+	total_calories: 0,
+	total_price: 0.00,
+	content: [
+	    {
+		"name": "chips",
+		"calories": 500,
+		"price": 1.00
+	    },
+	    {
+		"name": "tacos",
+		"calories": 1000,
+		"price": 4.00
+	    },
+	    {
+		"name": "cereal",
+		"calories": 300,
+		"price": 0.75
+	    },
+	    {
+		"name": "sandwich",
+		"calories": 600,
+		"price": 1.04
+	    },
+	    {
+		"name": "peanut butter",
+		"calories": 666,
+		"price": 0.42
+	    },
+	    {
+		"name": "beer",
+		"calories": 1647,
+		"price": 13.39
+	    }
+	]
+    },
+    {
+	id: 4,
+	total_calories: 0,
+	total_price: 0.00,
+	content: [
+	    {
+		"name": "pop",
+		"calories": 290,
+		"price": 1.00
+	    },
+	    {
+		"name": "pizza",
+		"calories": 1440,
+		"price": 2.50
+	    },
+	    {
+		"name": "tuna",
+		"calories": 80,
+		"price": 0.69
+	    },
+	    {
+		"name": "cereal",
+		"calories": 250,
+		"price": 0.23
+	    },
+	    {
+		"name": "sandwich",
+		"calories": 300,
+		"price": 0.52
+	    },
+	    {
+		"name": "peanut butter",
+		"calories": 666,
+		"price": 0.42
+	    },
+	    {
+		"name": "beer",
+		"calories": 1140,
+		"price": 5.98
+	    }
+	]
+    },
+    {
+	id: 5,
+	total_calories: 0,
+	total_price: 0.00,
+	content: [
+	    {
+		"name": "donuts",
+		"calories": 280,
+		"price": 0.32
+	    },
+	    {
+		"name": "fries",
+		"calories": 660,
+		"price": 0.75
+	    },
+	    {
+		"name": "ham",
+		"calories": 800,
+		"price": 1.86
+	    },
+	    {
+		"name": "tuna",
+		"calories": 80,
+		"price": 0.69
+	    },
+	    {
+		"name": "cereal",
+		"calories": 750,
+		"price": 0.69
+	    },
+	    {
+		"name": "sandwich",
+		"calories": 300,
+		"price": 0.52
+	    }
+	]
+    },
+    {
+	id: 6,
+	total_calories: 0,
+	total_price: 0.00,
+	content: [
+	    {
+		"name": "donuts",
+		"calories": 280,
+		"price": 0.32
+	    },
+	    {
+		"name": "cereal",
+		"calories": 300,
+		"price": 0.75
+	    },
+	    {
+		"name": "eggs",
+		"calories": 240,
+		"price": 0.15
+	    },
+	    {
+		"name": "peanut butter",
+		"calories": 1332,
+		"price": 0.84
+	    },
+	    {
+		"name": "beer",
+		"calories": 1554,
+		"price": 4.78
+	    }
+	]
+    },
+    {
+	id: 7,
+	total_calories: 0,
+	total_price: 0.00,
+	content: [
+	    {
+		"name": "donuts",
+		"calories": 280,
+		"price": 0.32
+	    },
+	    {
+		"name": "pop",
+		"calories": 290,
+		"price": 1.00
+	    },
+	    {
+		"name": "pizza",
+		"calories": 1440,
+		"price": 2.50
+	    },
+	    {
+		"name": "tuna",
+		"calories": 80,
+		"price": 0.69
+	    }
+	]
+    },
+    {
+	id: 8,
+	total_calories: 0,
+	total_price: 0.00,
+	content: [
+	    {
+		"name": "tuna",
+		"calories": 240,
+		"price": 2.07
+	    },
+	    {
+		"name": "eggs",
+		"calories": 240,
+		"price": 0.15
+	    },
+	    {
+		"name": "toast",
+		"calories": 250,
+		"price": 0.28
+	    },
+	    {
+		"name": "turkey sausage",
+		"calories": 250,
+		"price": 1.49
+	    },
+	    {
+		"name": "peanut butter",
+		"calories": 666,
+		"price": 0.42
+	    },
+	    {
+		"name": "beer",
+		"calories": 1197,
+		"price": 4.69
+	    }
+	]
+    },
+    {
+	id: 9,
+	total_calories: 0,
+	total_price: 0.00,
+	content: [
+	    {
+		"name": "pop",
+		"calories": 160,
+		"price": 1.00
+	    },
+	    {
+		"name": "ham",
+		"calories": 800,
+		"price": 1.86
+	    },
+	    {
+		"name": "soup",
+		"calories": 385,
+		"price": 1.13
+	    },
+	    {
+		"name": "rice",
+		"calories": 640,
+		"price": 0.35
+	    },
+	    {
+		"name": "eggs",
+		"calories": 240,
+		"price": 0.15
+	    },
+	    {
+		"name": "toast",
+		"calories": 500,
+		"price": 0.56
+	    },
+	    {
+		"name": "turkey sausage",
+		"calories": 250,
+		"price": 1.49
+	    }
+	]
+    },
+    {
+	id: 10,
+	total_calories: 0,
+	total_price: 0.00,
+	content: [
+	    {
+		"name": "chips",
+		"calories": 500,
+		"price": 1.00
+	    },
+	    {
+		"name": "tacos",
+		"calories": 1000,
+		"price": 4.00
+	    },
+	    {
+		"name": "noodles",
+		"calories": 380,
+		"price": 0.99
+	    },
+	    {
+		"name": "peanut butter",
+		"calories": 666,
+		"price": 0.42
+	    },
+	    {
+		"name": "beer",
+		"calories": 1997,
+		"price": 16.83
+	    }
+	]
+    },
+    {
+	id: 11,
+	total_calories: 0,
+	total_price: 0.00,
+	content: [
+	    {
+		"name": "cereal",
+		"calories": 300,
+		"price": 0.75
+	    },
+	    {
+		"name": "peanut butter",
+		"calories": 666,
+		"price": 0.42
+	    },
+	    {
+		"name": "beer",
+		"calories": 1450,
+		"price": 7.08
+	    }
+	]
+    },
+    {
+	id: 12,
+	total_calories: 0,
+	total_price: 0.00,
+	content: [
+	    {
+		"name": "enchiladas",
+		"calories": 1730,
+		"price": 4.15
+	    },
+	    {
+		"name": "rice",
+		"calories": 640,
+		"price": 0.35
+	    },
+	    {
+		"name": "sandwich",
+		"calories": 300,
+		"price": 0.52
+	    }
+	]
+    },
+    {
+	id: 13,
+	total_calories: 0,
+	total_price: 0.00,
+	content: [
+	    {
+		"name": "pop",
+		"calories": 290,
+		"price": 1.75
+	    },
+	    {
+		"name": "enchiladas",
+		"calories": 1730,
+		"price": 4.15
+	    },
+	    {
+		"name": "tuna",
+		"calories": 80,
+		"price": 0.69
+	    },
+	    {
+		"name": "rice",
+		"calories": 640,
+		"price": 0.35
+	    },
+	    {
+		"name": "noodles",
+		"calories": 380,
+		"price": 0.99
+	    },
+	    {
+		"name": "sandwich",
+		"calories": 300,
+		"price": 0.52
+	    }
+	]
+    },
+    {
+	id: 14,
+	total_calories: 0,
+	total_price: 0.00,
+	content: [
+	    {
+		"name": "pizza",
+		"calories": 1550,
+		"price": 3.00
+	    },
+	    {
+		"name": "sandwich",
+		"calories": 300,
+		"price": 0.52
+	    },
+	    {
+		"name": "peanut butter",
+		"calories": 666,
+		"price": 0.42
+	    },
+	    {
+		"name": "beer",
+		"calories": 777,
+		"price": 2.39
+	    }
+	]
+    },
+    {
+	id: 15,
+	total_calories: 0,
+	total_price: 0.00,
+	content: [
+	    {
+		"name": "pizza",
+		"calories": 1550,
+		"price": 3.00
+	    },
+	    {
+		"name": "cereal",
+		"calories": 300,
+		"price": 0.75
+	    },
+	    {
+		"name": "beer",
+		"calories": 1170,
+		"price": 12.30
+	    }
+	]
+    },
+    {
+	id: 16,
+	total_calories: 0,
+	total_price: 0.00,
+	content: [
+	    {
+		"name": "ham",
+		"calories": 800,
+		"price": 1.86
+	    },
+	    {
+		"name": "cereal",
+		"calories": 300,
+		"price": 0.75
+	    },
+	    {
+		"name": "beer",
+		"calories": 1170,
+		"price": 12.30
+	    }
+	]
+    },
+
+];
+
+var calories_april = [
+//march_calories = [
+    {
+	id: 1,
+	total_calories: 0,
+	total_price: 0.00,
+	content: [
+	    {
+		"name": "donuts",
+		"calories": 910,
+		"price": 0.96
+	    },
+	    {
+		"name": "pizza",
+		"calories": 1120,
+		"price": 2.50
+	    },
+	    {
+		"name": "cereal",
+		"calories": 250,
+		"price": 0.23
+	    },
+	    {
+		"name": "sandwich",
+		"calories": 300,
+		"price": 0.52
+	    },
+	    {
+		"name": "beer",
+		"calories": 1472,
+		"price": 10.75
+	    }
+	]
+    },
+    {
+	id: 2,
+	total_calories: 0,
+	total_price: 0.00,
+	content: [
+	    {
+		"name": "donuts",
+		"calories": 350,
+		"price": 0.32
+	    },
+	    {
+		"name": "fries",
+		"calories": 660,
+		"price": 0.75
+	    },
+	    {
+		"name": "pizza",
+		"calories": 2160,
+		"price": 6.57
+	    },
+	    {
+		"name": "sandwich",
+		"calories": 300,
+		"price": 0.52
+	    },
+	    {
+		"name": "beer",
+		"calories": 540,
+		"price": 3.80
+	    }
+	]
+    },
+    {
+	id: 3,
+	total_calories: 0,
+	total_price: 0.00,
+	content: [
+	    {
+		"name": "chips",
+		"calories": 500,
+		"price": 1.00
+	    },
+	    {
+		"name": "tacos",
+		"calories": 1000,
+		"price": 4.00
+	    },
+	    {
+		"name": "cereal",
+		"calories": 300,
+		"price": 0.75
+	    },
+	    {
+		"name": "sandwich",
+		"calories": 600,
+		"price": 1.04
+	    },
+	    {
+		"name": "peanut butter",
+		"calories": 666,
+		"price": 0.42
+	    },
+	    {
+		"name": "beer",
+		"calories": 1647,
+		"price": 13.39
+	    }
+	]
+    }
+];
+
+var calories_may = [
+//march_calories = [
+    {
+	id: 1,
+	total_calories: 0,
+	total_price: 0.00,
+	content: [
+	    {
+		"name": "donuts",
+		"calories": 910,
+		"price": 0.96
+	    },
+	    {
+		"name": "pizza",
+		"calories": 1120,
+		"price": 2.50
+	    },
+	    {
+		"name": "cereal",
+		"calories": 250,
+		"price": 0.23
+	    },
+	    {
+		"name": "sandwich",
+		"calories": 300,
+		"price": 0.52
+	    },
+	    {
+		"name": "beer",
+		"calories": 1472,
+		"price": 10.75
+	    }
+	]
+    },
+    {
+	id: 2,
+	total_calories: 0,
+	total_price: 0.00,
+	content: [
+	    {
+		"name": "donuts",
+		"calories": 350,
+		"price": 0.32
+	    },
+	    {
+		"name": "fries",
+		"calories": 660,
+		"price": 0.75
+	    },
+	    {
+		"name": "pizza",
+		"calories": 2160,
+		"price": 6.57
+	    },
+	    {
+		"name": "sandwich",
+		"calories": 300,
+		"price": 0.52
+	    },
+	    {
+		"name": "beer",
+		"calories": 540,
+		"price": 3.80
+	    }
+	]
+    },
+    {
+	id: 3,
+	total_calories: 0,
+	total_price: 0.00,
+	content: [
+	    {
+		"name": "chips",
+		"calories": 500,
+		"price": 1.00
+	    },
+	    {
+		"name": "tacos",
+		"calories": 1000,
+		"price": 4.00
+	    },
+	    {
+		"name": "cereal",
+		"calories": 300,
+		"price": 0.75
+	    },
+	    {
+		"name": "sandwich",
+		"calories": 600,
+		"price": 1.04
+	    },
+	    {
+		"name": "peanut butter",
+		"calories": 666,
+		"price": 0.42
+	    },
+	    {
+		"name": "beer",
+		"calories": 1647,
+		"price": 13.39
+	    }
+	]
+    },
+    {
+	id: 4,
+	total_calories: 0,
+	total_price: 0.00,
+	content: [
+	    {
+		"name": "pop",
+		"calories": 290,
+		"price": 1.00
+	    },
+	    {
+		"name": "pizza",
+		"calories": 1440,
+		"price": 2.50
+	    },
+	    {
+		"name": "tuna",
+		"calories": 80,
+		"price": 0.69
+	    },
+	    {
+		"name": "cereal",
+		"calories": 250,
+		"price": 0.23
+	    },
+	    {
+		"name": "sandwich",
+		"calories": 300,
+		"price": 0.52
+	    },
+	    {
+		"name": "peanut butter",
+		"calories": 666,
+		"price": 0.42
+	    },
+	    {
+		"name": "beer",
+		"calories": 1140,
+		"price": 5.98
+	    }
+	]
+    }
+];
